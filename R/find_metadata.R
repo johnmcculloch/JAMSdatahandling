f3dd070dc25b15e39076c3db63f5299a3830142e
johnmcculloch <john.anthony.mcculloch@gmail.com>
2020-08-19 12:46:36 -0400

    Cleanup and initial commit of smart JAMSalpha launcher

--- R/find_metadata.R
+++
@@ -1,8 +0,0 @@
-find_metadata <- function(path = NULL){
-    putativeSpreadsheets <- list.files(path = path, pattern = ".xlsx$")
-    putativeTables <- list.files(path = path, pattern = ".tsv$")
-
-    list.metadata <- list()
-
-    return(list.metadata)
-}
